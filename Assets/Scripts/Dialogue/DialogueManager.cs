575ccf7b36627ccba19d94cdf04d28e80c495d2b
M Farrel Danendra Rachim <13521048@std.stei.itb.ac.id>
2024-04-23 07:15:39 +0700

    feat: add dialoguemanager

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class DialogueManager : MonoBehaviour
 {
+    private Queue<string> sentences;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sentences = new Queue<string>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartDialogue(List<Dialogue> dialogues)
     {
-        
+
     }
+
 }
