d8d94a6d34716dcb47c941b2a9b3c442aab7a2c2
M Farrel Danendra Rachim <13521048@std.stei.itb.ac.id>
2024-04-23 12:37:24 +0700

    feat: complete opening scene

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -1,19 +1,79 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
-    private Queue<string> sentences;
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+    AudioSource dialogueAudio;
+
+    public Animator animator;
+    private Queue<Dialogue> dialogueQueue;
+
+    [Header("Audio")]
+    [SerializeField] private AudioClip dialogueTypingSoundClip;
+
+    void Awake()
+    {
+        dialogueAudio = this.gameObject.AddComponent<AudioSource>();
+    }
     // Start is called before the first frame update
     void Start()
     {
-        sentences = new Queue<string>();
+        dialogueQueue = new Queue<Dialogue>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            DisplayNextSentence();
+        }
     }
 
     public void StartDialogue(List<Dialogue> dialogues)
     {
+        animator.SetBool("IsOpen", true);
+        dialogueQueue.Clear();
+        foreach (Dialogue dialogue in dialogues)
+        {
+            dialogueQueue.Enqueue(dialogue);
+        }
 
+        DisplayNextSentence();
     }
 
+    public void DisplayNextSentence()
+    {
+        if (dialogueQueue.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        Dialogue nextDialogue = dialogueQueue.Dequeue();
+        nameText.text = nextDialogue.name;
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(nextDialogue.sentences));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueAudio.PlayOneShot(dialogueTypingSoundClip);
+            dialogueText.text += letter;
+            yield return new WaitForSeconds((float)0.015);
+        }
+    }
+
+    void EndDialogue()
+    {
+        animator.SetBool("IsOpen", false);
+    }
 }
