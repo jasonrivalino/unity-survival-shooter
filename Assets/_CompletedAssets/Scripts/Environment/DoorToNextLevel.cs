904720d776b400974d12c130b3a2ef10763e7e93
M Farrel Danendra Rachim <13521048@std.stei.itb.ac.id>
2024-04-24 21:59:40 +0700

    feat: add interaction features

---
+++ Assets/_CompletedAssets/Scripts/Environment/DoorToNextLevel.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorToNextLevel : MonoBehaviour
+{
+    public void Interact()
+    {
+        Debug.Log("Door to next level");
+    }
+}
--- Assets/_CompletedAssets/Scripts/Player/PlayerInteractor.cs
+++ Assets/_CompletedAssets/Scripts/Player/PlayerInteractor.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -8,10 +10,13 @@ public class PlayerInteractor : MonoBehaviour
     public bool isInRange;
     public KeyCode interactKey;
     public UnityEvent interactAction;
+    public GameObject visualUI;
+    public TextMeshProUGUI hintName;
+    public String visualObjectAction;
     // Start is called before the first frame update
     void Start()
     {
-
+        visualUI.SetActive(false);
     }
 
     // Update is called once per frame
@@ -30,6 +35,8 @@ public class PlayerInteractor : MonoBehaviour
     {
         if (collider.gameObject.CompareTag("Player"))
         {
+            visualUI.SetActive(true);
+            hintName.text = "Tekan E untuk " + visualObjectAction + "!";
             isInRange = true;
             Debug.Log("Player is in range");
         }
@@ -39,6 +46,7 @@ public class PlayerInteractor : MonoBehaviour
     {
         if (collider.gameObject.CompareTag("Player"))
         {
+            visualUI.SetActive(false);
             isInRange = false;
             Debug.Log("Player is not in range");
         }
