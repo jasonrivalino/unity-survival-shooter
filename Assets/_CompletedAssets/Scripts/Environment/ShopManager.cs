be6bf6635a8deed15b7219d1eea0e89ff99c9691
bintang433 <13521003@std.stei.itb.ac.id>
2024-05-01 22:58:17 +0700

    Revert "feat: make shopcanvas disappear"
    This reverts commit 5cce7b26ca47554618b89a2d0a3b3d7d43ac43f5.

--- Assets/_CompletedAssets/Scripts/Environment/ShopManager.cs
+++ Assets/_CompletedAssets/Scripts/Environment/ShopManager.cs
@@ -5,23 +5,15 @@ using UnityEngine;
 public class ShopManager : MonoBehaviour
 {
     public GameObject shop;
-    Canvas canvas;
-    public GameObject interactHint;
-
     // Start is called before the first frame update
     void Start()
     {
-        GameObject shopCanvas = GameObject.Find("ShopCanvas");
-        canvas = shopCanvas.GetComponent<Canvas>();
-        shop = GameObject.Find("shop");
-        StartCoroutine(DisappearShop());
+        // StartCoroutine(DisappearShop());
     }
 
     IEnumerator DisappearShop()
     {
-        yield return new WaitForSeconds(30);
+        yield return new WaitForSeconds(25);
         shop.SetActive(false);
-        canvas.enabled = false;
-        interactHint.SetActive(false);
     }
 }
