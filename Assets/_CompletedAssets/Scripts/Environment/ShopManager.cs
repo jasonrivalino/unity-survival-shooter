46ac205b8af4c4797d1def63f5a9ff42312003fb
M Farrel Danendra Rachim <13521048@std.stei.itb.ac.id>
2024-05-01 13:58:10 +0700

    feat: add town disappear

---
+++ Assets/_CompletedAssets/Scripts/Environment/ShopManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+    public GameObject shop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DisappearShop());
+    }
+
+    IEnumerator DisappearShop()
+    {
+        yield return new WaitForSeconds(25);
+        shop.SetActive(false);
+    }
+}
