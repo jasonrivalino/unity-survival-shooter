3f2cb0fbe7de6363c8c88e461ce557d5bcbb17e4
M Farrel Danendra Rachim <13521048@std.stei.itb.ac.id>
2024-05-02 14:02:47 +0700

    feat: add money manager

---
+++ Assets/_CompletedAssets/Scripts/Managers/MoneyManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MoneyManager : MonoBehaviour
+{
+    public static int money;
+    public TMP_Text moneyPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "Town1")
+        {
+            money = 0;
+            money += 100;
+        }
+        else if (SceneManager.GetActiveScene().name == "Town2")
+        {
+            money += 300;
+        }
+        else if (SceneManager.GetActiveScene().name == "Town3")
+        {
+            money += 500;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moneyPanel.text = money.ToString();
+    }
+}
