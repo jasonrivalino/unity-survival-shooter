b7c5d383812d06ec8081d03a9157df6b5bab9cf7
jasonrivalino <13521008@std.stei.itb.ac.id>
2024-05-09 10:31:00 +0700

    fix: spawn zombear on hellephant

--- Assets/_CompletedAssets/Scripts/Managers/EnemyHellephantManager.cs
+++ Assets/_CompletedAssets/Scripts/Managers/EnemyHellephantManager.cs
@@ -4,20 +4,17 @@ namespace CompleteProject
 {
     public class EnemyHellephantManager : MonoBehaviour
     {
-        public PlayerHealth playerHealth;       // Reference to the player's health.
-        public Transform hellephantTransform;  // Reference to the hellephant's transform.
-        public GameObject zomBear;             // The enemy prefab to be spawned.
-        public float spawnRadius = 5f;
-        public float spawnCollisionCheckRadius = 0.5f;
-        public float spawnTime = 3f;           // How long between each spawn.
+        PlayerHealth playerHealth;       // Reference to the player's heatlh.
+        public UnityEngine.GameObject enemy;                // The enemy prefab to be spawned.
+        public float spawnTime = 3f;            // How long between each spawn.
         public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
 
         void Start()
         {
             // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
             InvokeRepeating("Spawn", spawnTime, spawnTime);
+            playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
         }
-
 
         void Spawn()
         {
@@ -28,40 +25,11 @@ namespace CompleteProject
                 return;
             }
 
-            // Generate Random Point for Spawn
-            Vector3 spawnPoint = Vector3.zero;
-            spawnPoint.y = spawnCollisionCheckRadius + 0.01f;
+            // Find a random index between zero and one less than the number of spawn points.
+            int spawnPointIndex = Random.Range(0, spawnPoints.Length);
 
-            // Get random point
-            spawnPoint.x = hellephantTransform.position.x + (2 * Random.value - 1) * spawnRadius;
-            spawnPoint.z = hellephantTransform.position.z + (2 * Random.value - 1) * spawnRadius;
-
-            // Adjust to not exit the orb spawn radius
-            if (spawnPoint.x < 0)
-            {
-                spawnPoint.x = Mathf.Max(spawnPoint.x, -spawnRadius);
-            }
-            else if (spawnPoint.x > 0)
-            {
-                spawnPoint.x = Mathf.Min(spawnPoint.x, spawnRadius);
-            }
-            if (spawnPoint.z < 0)
-            {
-                spawnPoint.z = Mathf.Max(spawnPoint.z, -spawnRadius);
-            }
-            else if (spawnPoint.z > 0)
-            {
-                spawnPoint.z = Mathf.Min(spawnPoint.z, spawnRadius);
-            }
-
-            if (!Physics.CheckSphere(spawnPoint, spawnCollisionCheckRadius))
-            {
-                // Find a random index between zero and one less than the number of spawn points.
-                int spawnPointIndex = Random.Range(0, spawnPoints.Length);
-
-                // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
-                Instantiate(zomBear, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
-            }
+            // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+            Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
         }
     }
 }
