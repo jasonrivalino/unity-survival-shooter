5b14d5dab00c4a5ad797d178a6f6c77b4e1581cc
akmaldika <13521070@mahasiswa.itb.ac.id>
2024-05-11 10:59:24 +0700

    feat: add SaveMenu script

--- Assets/_CompletedAssets/Scripts/Environment/SaveMenu.cs
+++ Assets/_CompletedAssets/Scripts/Environment/SaveMenu.cs
@@ -1,9 +1,6 @@
 using System.Collections.Generic;
-using System.Xml.Serialization;
 using CompleteProject;
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class SaveMenu : MonoBehaviour {
     [Header("Confirmation Dialog")]
@@ -19,7 +16,7 @@ public class SaveMenu : MonoBehaviour {
 
     private void Start()
     {
-        ActivareMenu();
+        ActivateMenu();
     }
 
     private void DisableInteraction() 
@@ -38,50 +35,92 @@ public class SaveMenu : MonoBehaviour {
         }
     }
 
+    private GameData GetGameData() {
+        return new GameData(
+            slotName: slotnNameInput.slotNameInput.text,
+            sceneName: UnityEngine.SceneManagement.SceneManager.GetActiveScene().name,
+            playerName: PlayerPrefs.GetString("PlayerName", "Player1"),
+            saveTime: System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
+            money: PlayerPrefs.GetInt(Statistics.MoneyCollected, 0),
+            score: PlayerPrefs.GetInt(Statistics.TotalScore, 0),
+            playTime: PlayerPrefs.GetFloat(Statistics.PlayTime, 0),
+            accuracy: PlayerPrefs.GetFloat(Statistics.ShootAccuracy, 0),
+            distanceTravelled: PlayerPrefs.GetFloat(Statistics.DistanceTravelled, 0),
+            enemiesKilled: PlayerPrefs.GetInt(Statistics.EnemiesKilled, 0),
+            orbCollected: PlayerPrefs.GetInt(Statistics.OrbCollected, 0),
+            pets: new Dictionary<string, int>()
+            {
+                { Pets.Rabbit, PlayerPrefs.GetInt(Pets.Rabbit, 0) },
+                { Pets.Mushroom, PlayerPrefs.GetInt(Pets.Mushroom, 0) },
+                { Pets.Ghost, PlayerPrefs.GetInt(Pets.Ghost, 0) },
+                { Pets.Dog, PlayerPrefs.GetInt(Pets.Dog, 0) },
+                { Pets.Cactus, PlayerPrefs.GetInt(Pets.Cactus, 0) },
+                { Pets.Bomb, PlayerPrefs.GetInt(Pets.Bomb, 0) }
+            }
+        );
+        /* 
+        public static class Pets {
+        public const string Rabbit = "rabbit";
+        public const string Mushroom = "mushroom";
+        public const string Ghost = "ghost";
+        public const string Dog = "dog";
+        public const string Cactus = "cactus";
+        public const string Bomb = "bomb";
+        }
+         */
+    }
+
+    public void OnSaveSlotBeforeSave(SaveSlot saveSlot) {
+        slotnNameInput.ActivateDialog(
+            "Enter Slot Name",
+            "Save",
+            () => {
+                GameData gameData = GetGameData();
+
+                SaveManager.instance.SavePlayer(gameData, saveSlot.ProfileId);
+
+                saveSlot.SetData(SaveManager.instance.LoadPlayer(saveSlot.ProfileId));
+            }
+        );
+    }
+
     public void OnSaveSlotClicked() 
     {
         SaveSlot saveSlot = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.GetComponent<SaveSlot>();
 
         if (saveSlot.hasData) 
         {
-            // confirmationDialog.ActivateDialog (
-            //     "Load this save?", "Load", "Cancel",
-            //      () => {
-            //         LoadGame(saveSlot.ProfileId);
-            //      },
-            //      () => {
-            //         EnableInteraction();
-            //      }
-            // );
+
+            // confirmto overwrite then ask like save to this slot
+
+            DisableInteraction();
+            confirmationDialog.ActivateDialog (
+                "Overwrite this slot?", "Overwrite", "Cancel",
+                 () => {
+                    SaveManager.instance.DeletePlayer(saveSlot.ProfileId);
+                    // ask for slot name
+                    OnSaveSlotBeforeSave(saveSlot);
+
+                    ActivateMenu();
+                    EnableInteraction();
+
+                 },
+                 () => {
+                    EnableInteraction();
+                 }
+            );
+
         }
         else 
         {
+            DisableInteraction();
             confirmationDialog.ActivateDialog (
                 "Save to this slot?", "Save", "Cancel",
                  () => {
-                    DisableInteraction();
                     // ask for slot name
-                    slotnNameInput.ActivateDialog(
-                        "Enter Slot Name",
-                        "Save",
-                        () => {
-                            SaveManager.instance.SavePlayer(new GameData(
-                                string.IsNullOrEmpty(slotnNameInput.slotNameInput.text) || string.IsNullOrWhiteSpace(slotnNameInput.slotNameInput.text) ? saveSlot.GetSlotName() : slotnNameInput.slotNameInput.text,
-                                PlayerPrefs.GetString("PlayerName", "Player1"),
-                                0,
-                                0,
-                                UnityEngine.SceneManagement.SceneManager.GetActiveScene().name,
-                                System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
-                                (System.DateTime.Now - System.DateTime.Now.AddMinutes(-10)).ToString()
-                                // new string[] { "dog", "rabbit", "mushroom", "ghost", "cactus", "bomb" }
-                                
-                                ), saveSlot.ProfileId);
+                    OnSaveSlotBeforeSave(saveSlot);
 
-                            saveSlot.SetData(SaveManager.instance.LoadPlayer(saveSlot.ProfileId));
-                        }
-                    );
-
-                    ActivareMenu();
+                    ActivateMenu();
                     EnableInteraction();
 
                  },
@@ -98,7 +137,7 @@ public class SaveMenu : MonoBehaviour {
         DisableInteraction();
     }
         
-    public void ActivareMenu()
+    public void ActivateMenu()
     {
         gameObject.SetActive(true);
 
@@ -109,7 +148,7 @@ public class SaveMenu : MonoBehaviour {
             if (players.ContainsKey(saveSlot.ProfileId)) 
             {
                 saveSlot.SetData(players[saveSlot.ProfileId]);
-                Debug.Log("SaveMenu::ActivareMenu() " + players[saveSlot.ProfileId]);
+                Debug.Log("SaveMenu::ActivateMenu() " + players[saveSlot.ProfileId]);
             }
             else 
             {
