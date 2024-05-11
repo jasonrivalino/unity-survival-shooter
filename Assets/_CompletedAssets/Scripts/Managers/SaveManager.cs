deaadd863066326dacbbc49931ac5d0dd4d20075
akmaldika <13521070@mahasiswa.itb.ac.id>
2024-05-11 11:02:02 +0700

    feat: save and load manager - save and load manager file are singelton - with base saveload

--- Assets/_CompletedAssets/Scripts/Managers/SaveManager.cs
+++ Assets/_CompletedAssets/Scripts/Managers/SaveManager.cs
@@ -1,20 +1,13 @@
 // SaveManager.cs
 // MelakukanSavePlayer
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
 
 namespace CompleteProject
 {
-    public class SaveManager : MonoBehaviour
+    public class SaveManager : SaveLoad
     {
         public static SaveManager instance;
 
-        public string dirPath {get; private set;}
-
-        public string fileName {get; private set;} = "PData.dat";
         private void Awake()
         {
             if (instance == null)
@@ -31,43 +24,6 @@ namespace CompleteProject
             {
                 Destroy(gameObject);
             }
-        }
-
-
-        public void SavePlayer(GameData gameData, string profileId)
-        {
-            DataFileHandler.SavePlayer(gameData, dirPath, fileName, profileId);
-        }
-
-        public GameData LoadPlayer(string profileId)
-        {
-            /*
-            Data yang harus diload di sini:
-            - level / scene yang nanti dimainkan v
-            - nama player v
-            - uang player v
-            - score player v
-            - waktu save 
-            - waktu bermain
-            - slot name
-            */
-            GameData gameData = DataFileHandler.LoadPlayer(dirPath, fileName, profileId);   
-            
-            PlayerPrefs.SetString("playerName", gameData.playerName);
-            
-            MoneyManager.money = gameData.money;
-            ScoreManager.score = gameData.score;
-
-            PlayerPrefs.SetString("startTime", gameData.saveTime);
-            PlayerPrefs.SetString("playedTime", gameData.playTime);
-            
-            Debug.Log("SaveManager::LoadPlayer() " + gameData.playerName + " loaded");
-            return gameData;
-        }
-
-        public void DeletePlayer(string profileId)
-        {
-            DataFileHandler.DeletePlayer(dirPath, fileName, profileId);
         }
     }
 }
